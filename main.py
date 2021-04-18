c8bd8ae811d9039332a01a51b7035bf078ed7801
ROCCYK <82534598+ROCCYK@users.noreply.github.com>
2021-04-18 17:08:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#BMI calculator
+
+name = "Giannis Antetokounmpo"
+height_feet = 6
+height_inches = 11
+height_feet_inches = (height_feet * 12) + height_inches
+weight_lbs = 230
+bmi = ((weight_lbs/height_feet_inches ** 2) * 703)
+
+print(name)
+
+if bmi >= 25:
+    print("You Are Overweight")
+elif bmi <= 18.5:
+    print("You Are Underweight")
+else:
+    print("Your bmi is normal")
+print("Your BMI is:")
+print(round(bmi, 2))
